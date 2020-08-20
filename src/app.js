09326381b7d3d1910054d5b49ceb151dfa130fe5
chkrishnamurthy <chalapatikrishna@gmail.com>
2020-08-20 18:19:05 +0530

    setup for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const hbs = require("hbs");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
+const port = process.env.PORT || 3000;
+console.log(port);
+
 //Define paths forExpres config
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewPath = path.join(__dirname, "../templates/views");
@@ -90,6 +93,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server is started..!");
 });
